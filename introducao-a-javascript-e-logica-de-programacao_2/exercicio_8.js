1b3a6b1f938e54e650acd545f0b162a376ba422a
Emmanuel <EmmanuelRodriguesBoo@gmail.com>
2021-06-25 16:55:49 -0300

    Exercícios 4.2

---
+++ introducao-a-javascript-e-logica-de-programacao_2/exercicio_8.js
@@ -0,0 +1,7 @@
+let num = [];
+
+for(let i = 0; i <= 25; i += 1){
+  num.push(i)
+}
+
+console.log(num)
---
+++ introducao-a-javascript-e-logica-de-programacao_2/exercicio_9.js
@@ -0,0 +1,8 @@
+let num = [];
+
+for(let i = 1; i <= 25; i += 1){
+  let div = i /2;
+  num.push(div);
+}
+
+console.log(num)
